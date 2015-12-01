eee3c87ea47fb4a18b44fef36d154a7d663f3c23
JonathanHunter <info@lostsouth.co.za>
2015-12-01 16:38:28 +0200

    Sorted out bugs that where stopping the skills element from loading
    added some css changes
    
    Did not commit often enough. /

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,110 +1,110 @@
 // *** Creating objects *** - bio, work, projects, education
 
 var bio = {
-	"name": "Jonathan Hunter",
-	"role": "Front-end Developer",
-	"contacts": {
-		"email": "me@jonathanhunter.co.za",
-		"mobile": "083 450 6397",
-		"twitter": "@JON4TH4NHUNT3R",
-		"github": "https://github.com/JON4TH4NHUNT3R",
+    "name": "Jonathan Hunter",
+    "role": "Front-end Developer",
+    "contacts": {
+        "email": "me@jonathanhunter.co.za",
+        "mobile": "083 450 6397",
+        "twitter": "@JON4TH4NHUNT3R",
+        "github": "https://github.com/JON4TH4NHUNT3R",
         "linkedin": "linkedin.com",
-		"location": "Cape Town, South Africa"
+        "location": "Cape Town, South Africa"
     },
-	"imgURL": "images/me.jpg",
-	"welcome": "<strong>Welcome to my Udacity sponsored resume!</strong> All the information you see here is stored in JSON objects with javascript building the page on the fly. It's here to show that I can use some basic javascript front end techniques. The source can be found on <a href='https://github.com/JON4TH4NHUNT3R/frontend-nanodegree-resume.git'>github project page</a>.",
-	"skills": ["HTML", "CSS", "JavaScript", "Awesome Sauce" ]
+    "imgURL": "images/140.jpg",
+    "welcome": "<strong>Welcome to my resume!</strong> All the information you see here is stored in JSON objects with javascript building the page on the fly. It's here to show that I can use some basic javascript front end techniques. The source can be found on <a href='https://github.com/JON4TH4NHUNT3R/frontend-nanodegree-resume.git'>github project page</a>.",
+    "skills": ["HTML", "CSS", "JavaScript", "Awesome Sauce" ]
 };
 
 
 
 var work = {
-	"jobs": [
-		{
-			"employer": "Cars-4-Hire",
-			"title": "Web developer",
-			"location": "Cape Town, South Africa",
-			"dates": "2015 - Present",
-			"description": "Maintenance and design of the Cars-4-Hire website, responsible for updating content and optimizing the website in order to increase conversions"
-		},
-		{
-			"employer": "Zanzibar Organic Solutions",
-			"title": "Web Developer",
-			"location": "Zanzibar, Tanzania",
-			"dates": "2014-2015",
-			"description": "Oversaw the maintanence and improvement of Zanzibar Organic Solutions clients web pages"
-		},
-		{
-			"employer": "Grow Guru",
-			"title": "Web developer, SEO, Content creation",
-			"location": "Port Elizabeth, South Africa",
-			"dates": "2013-2014",
-			"description": "Created the responsive Grow Guru website, Perfomed ongoing SEO optimisations and produced content for the blog"
-		}
-	]
+    "jobs": [
+        {
+            "employer": "Cars-4-Hire",
+            "title": "Web developer",
+            "location": "Cape Town, South Africa",
+            "dates": "2015 - Present",
+            "description": "Maintenance and design of the Cars-4-Hire website, responsible for updating content and optimizing the website in order to increase conversions"
+        },
+        {
+            "employer": "Zanzibar Organic Solutions",
+            "title": "Web Developer",
+            "location": "Zanzibar, Tanzania",
+            "dates": "2014-2015",
+            "description": "Oversaw the maintanence and improvement of Zanzibar Organic Solutions clients web pages"
+        },
+        {
+            "employer": "Grow Guru",
+            "title": "Web developer, SEO, Content creation",
+            "location": "Port Elizabeth, South Africa",
+            "dates": "2013-2014",
+            "description": "Created the responsive Grow Guru website, Perfomed ongoing SEO optimisations and produced content for the blog"
+        }
+    ]
 };
 
 var projects = {
-	"projects": [
-		{
-			"title": "My Responsive Portfolio",
-			"description": "Responsive portfolio built with HTML, CSS and JS",
-			"dates": "2015",
-			"images": ["images/facebook.png"]
-		},
-		{
-			"title": "Frogger type game",
-			"description": "Frogger game built with JS",
-			"dates": "2015",
-			"images": ["images/facebook.png"]
-		},
-		{
-			"title": "Cars-4-Hire Responsive Design",
-			"description": "A responsive website built on Bootstrap framework using HTML, CSS, JS and PHP",
-			"dates": "2015",
-			"images": ["images/google.png"]
-		},
-	 	{
-			"title": "Grow Guru",
-			"description": "Created a wordpress theme, content creation, SEO optimization, Bitcoin integration",
-			"dates": "2013",
-			"images": ["images/facebook.png"]
-		},
-		{
-			"title": "Blockchain House Branding Package",
-			"description": "Created a full branding package that included business cards, logo, etc",
-			"dates": "2012",
-			"images": ["images/facebook.png"]
-		},
-	]
+    "projects": [
+        {
+            "title": "My Responsive Portfolio",
+            "description": "Responsive portfolio built with HTML, CSS and JS",
+            "dates": "2015",
+            "images": ["images/140.jpg"]
+        },
+        {
+            "title": "Frogger type game",
+            "description": "Frogger game built with JS",
+            "dates": "2015",
+            "images": ["images/140.jpg"]
+        },
+        {
+            "title": "Cars-4-Hire Responsive Design",
+            "description": "A responsive website built on Bootstrap framework using HTML, CSS, JS and PHP",
+            "dates": "2015",
+            "images": ["images/140.jpg"]
+        },
+        {
+            "title": "Grow Guru",
+            "description": "Created a wordpress theme, content creation, SEO optimization, Bitcoin integration",
+            "dates": "2013",
+            "images": ["images/140.jpg"]
+        },
+        {
+            "title": "Blockchain House Branding Package",
+            "description": "Created a full branding package that included business cards, logo, etc",
+            "dates": "2012",
+            "images": ["images/140.jpg"]
+        },
+    ]
 };
 
 var education = {
-	"schools": [
-		{
-			"name": "University of Cape Town",
-			"location": "Cape Town, South Africa",
-			"degree" : "BA of Music (Incomplete)",
-			"majors": ["Composition", "Electronic Production"],
-			"dates": "2006 - 2009",
+    "schools": [
+        {
+            "name": "University of Cape Town",
+            "location": "Cape Town, South Africa",
+            "degree" : "BA of Music (Incomplete)",
+            "majors": ["Composition", "Electronic Production"],
+            "dates": "2006 - 2009",
             "url" : "http://www.uct.co.za"
-		}],
-	"onlineCourses": [
-		{
-			"title": "Introduction to HTML",
-			"school": "Udacity",
-			"dates": "2015",
-			"url": "http://udacity.com",
+        }],
+    "onlineCourses": [
+        {
+            "title": "Introduction to HTML",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "http://udacity.com",
             "desc": "Learn how to convert digital design mockups into static web pages and how to build a responsive portfolio site to showcase your work."
-		},
-		{
-			"title": "JavaScript Basics",
-			"school": "Udacity",
-			"dates": "2015",
-			"url": "http://www.udacity.com",
+        },
+        {
+            "title": "JavaScript Basics",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "http://www.udacity.com",
             "desc": "Learn JavaScript syntax and coding conventions that web developers use to create interactive and dynamic websites while you create an online resume for your portfolio."
-		}
-	]
+        }
+    ]
 };
 var skills = {
     "skills": [
@@ -163,7 +163,7 @@ var skills = {
     // Append HTML
     $("#header").prepend(bioName); 
     $("#header").prepend(bioPic);
-    //$("#header").prepend(bioRole); // Removed
+ //   $("#header").prepend(bioRole); // Removed
     
     $("#header").append(bioWelcome);
 
@@ -174,7 +174,12 @@ var skills = {
     $("#topContacts").append(biolinkedin);
     $("#topContacts").append(bioLocation);
 
-
+  //  $("#header").append(HTMLskillsStart); 
+    
+    // Get skills bio data from the skills object
+ //   for (var i in skills.skills) {
+ //       $("#skills").append(HTMLskills.replace("%data%", skills.skills[i].name)); 
+  //  }
     
     $("#header").append(clearFix); 
 }
@@ -182,14 +187,14 @@ var skills = {
 work.display = function () {
     
     // Cycle through job objects and display each job
-	for (var i in work.jobs) {
-		$("#workExperience").append(HTMLworkStart);
-		var jobTitle = (HTMLworkEmployer.replace("%data%", work.jobs[i].employer) + HTMLworkTitle.replace("%data%", work.jobs[i]["title"]));
-		var jobLocation = HTMLworkLocation.replace("%data%", work.jobs[i]["location"]);
-		var jobDates = HTMLworkDates.replace("%data%", work.jobs[i]["dates"]);
-		var jobDesc = HTMLworkDescription.replace("%data%", work.jobs[i]["description"]);
-		$(".work-entry:last").append(jobTitle, jobLocation, jobDates, jobDesc);
-	}
+    for (var i in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        var jobTitle = (HTMLworkEmployer.replace("%data%", work.jobs[i].employer) + HTMLworkTitle.replace("%data%", work.jobs[i]["title"]));
+        var jobLocation = HTMLworkLocation.replace("%data%", work.jobs[i]["location"]);
+        var jobDates = HTMLworkDates.replace("%data%", work.jobs[i]["dates"]);
+        var jobDesc = HTMLworkDescription.replace("%data%", work.jobs[i]["description"]);
+        $(".work-entry:last").append(jobTitle, jobLocation, jobDates, jobDesc);
+    }
 
 }
 
@@ -231,14 +236,14 @@ projects.display = function () {
     $("#projects").append('<div class="project-entries">');
     
     // Cycle through each project and display
-	for (var i in projects.projects) {
-		$(".project-entries").append(HTMLprojectStart);
-		var projectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
-		var projectDesc = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
-		var projectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
-		var projectImg = HTMLprojectImage.replace("%data%", projects.projects[i].images);
-		$(".project-entry:last").append(projectTitle, projectDesc, projectDates, projectImg);
-	}
+    for (var i in projects.projects) {
+        $(".project-entries").append(HTMLprojectStart);
+        var projectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+        var projectDesc = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+        var projectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+        var projectImg = HTMLprojectImage.replace("%data%", projects.projects[i].images);
+        $(".project-entry:last").append(projectTitle, projectDesc, projectDates, projectImg);
+    }
 }
 
 skills.display = function () {
@@ -297,14 +302,6 @@ skills.display = function () {
         $('.skillInfo').addClass('hide');
         $('#' + skill + '-info').removeClass('hide');
     });
-    
-    /* 
-    // Original code to show skills info. Don't know why this didn't work. 
-    for (var i in skills.skills) {
-        $('#' + skills.skills[i].id + '-bar').click(function () {
-            $('#' + skills.skills[i].id + '-info').fadeIn(300);
-        });   
-    }*/
 
 };
 
@@ -317,13 +314,26 @@ skills.display();
 
 
 // *** Map ***
-var locationizer = function(work_obj) {
-	var locations = [];
-	for (var i in work_obj.jobs){
-		locations.push(work_obj.jobs[i]["location"]);
-	}
-	return locations;
+function locationizer (work_obj) {
+    var locationArray = [];
+    for (job in work_obj.jobs){
+        var newLocation = work_obj.jobs[job].location;
+        locationArray.push(newLocation);
+    }
+    return locationArray;
 }
+
+// **** Internationalize Name Button
+function inName (name) {
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0] + " " + name[1];
+}
+
+$('#header').prepend(internationalizeButton);
 
 $("#mapDiv").append(googleMap);
 
